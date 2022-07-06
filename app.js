4d8158fd1c77aa7977e942d0637f4ced68eb1f1a
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-07-06 01:09:40 -0700

    add inquirer pakage, gitignore, inquirer questions

---
+++ app.js
@@ -0,0 +1,86 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// inquirer questions
+
+const manager = [
+	{
+		type: 'input',
+		message: "What is the manager's name?",
+		name: 'name',
+		
+	},
+	{
+		type: 'input',
+		message: "What is the manager's ID?",
+		name: 'id',
+		
+	},
+	{
+		type: 'input',
+		message: "What is the manager's email?",
+		name: 'email',
+		
+	},
+	{
+		type: 'input',
+		message: "What is the manager's office number?",
+		name: 'officeNumber',
+		
+	}
+];
+
+const engineer = [
+	{
+		type: 'input',
+		message: "What is the engineer's name?",
+		name: 'name',
+		
+	},
+	{
+		type: 'input',
+		message: "What is the engineer's ID?",
+		name: 'id',
+		
+	},
+	{
+		type: 'input',
+		message: "What is the engineer's email?",
+		name: 'email',
+		
+	},
+	{
+		type: 'input',
+		message: "What is the engineer's GitHub username?",
+		name: 'github',
+		
+	}
+];
+
+const intern = [
+	{
+		type: 'input',
+		message: "What is the intern's name?",
+		name: 'name',
+		
+	},
+	{
+		type: 'input',
+		message: "What is the intern's ID?",
+		name: 'id',
+		
+	},
+	{
+		type: 'input',
+		message: "What is the intern's email?",
+		name: 'email',
+		
+	},
+	{
+		type: 'input',
+		message: "What is the intern's school?",
+		name: 'school',
+		
+	}
+];
+
