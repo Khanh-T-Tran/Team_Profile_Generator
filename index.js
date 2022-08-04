029aac68c142f2ec6eb3ac45a1c73cfc1fa6daf1
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-08-04 15:54:17 -0700

    edit question

--- index.js
+++ index.js
@@ -6,56 +6,35 @@ const fs = require('fs');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const { resolve } = require('path');
-const Choices = require('inquirer/lib/objects/choices');
+const questions = require('./lib/Questions');
+// const { resolve } = require('path');
+// const Choices = require('inquirer/lib/objects/choices');
 
 // emty array to input new team member in
 const teamArray = []; 
 
 // prompt manager
-const inputManager =   inquirer.prompt ([
-        {
-            type: 'input',
-		    message: "What is the manager's name?",
-		    name: 'name'
-        },
-        {
-            type: 'input',
-		    message: "What is the manager's ID?",
-		    name: 'id'
-        },
-        {
-            type: 'input',
-            message: "What is the manager's email?",
-            name: 'email'
-        },
-        {
-            type: 'input',
-		    message: "What is the manager's office number?",
-		    name: 'officeNumber'
-        }
-        
-    ])
-    .then (addManager => {
-        const {name, id, email, officeNumber} = addManager ;
-        const newManager = new Manager (name, id, email, officeNumber);
-        teamArray.push(newManager);
-        console.log(newManager);
-        // addEmployee()
-    });
+const inputManager = (type) => {
+    return inquirer.prompt(questions[type]);
+};
 
-function addEmployee () {
-    return inquirer.prompt([
-        {
-            message: 'Please choose a role to add!',
-            choices: ['Engineer', 'Intern', 'finish adding employee']
-        }
-       
-    ])
+//     .then (addManager => {
+//         // using Destructuring assignment to declare addManager
+//         const {name, id, email, officeNumber} = addManager ;
+//         const newManager = new Manager (name, id, email, officeNumber);
+//         teamArray.push(newManager);
+//         console.log(addManager);
+//         addEmployee();
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+// };
+// inputManager();
+
+// const addEmployee = () => {
     
-}
-    // const inputEngineer inquirer
-    // const inputIntern inquirer\
+// }
     
 
 
